f627560dfbc3dd52c906506c4895864508d7c610
Brian Alvey <github@subs.frankensite.com>
2021-05-24 22:31:57 -0700

    first

---
+++ src/index.worker.ts
@@ -0,0 +1,14 @@
+import { GetCustomDataDescriptorsFn, SiteData } from "@clipisode/theme";
+
+export const getCustomDataDescriptors: GetCustomDataDescriptorsFn = () => {
+  return [];
+};
+
+export const siteData: SiteData = {
+  customCssKey: "site.css",
+  brandColor: "#7df42e",
+  introScreen: {},
+  nameScreen: {},
+  emailScreen: {},
+  closedScreen: {},
+};
